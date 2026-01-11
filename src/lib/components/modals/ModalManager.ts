090d390d02470fcc99e8f3adb1a5b6cf68b1abab
sherifcodeur <sherifcodeur@gmail.com>
2026-01-11 23:10:37 +0100

    tool audio merge fonctionne mais rajoute dans assets pas sur timeline

--- src/lib/components/modals/ModalManager.ts
+++ src/lib/components/modals/ModalManager.ts
@@ -7,6 +7,7 @@ import NewUpdateModal from '../home/modals/NewUpdateModal.svelte';
 import DeleteConfirmation from './DeleteConfirmation.svelte';
 import ShiftSubtitlesModal from '../projectEditor/tabs/subtitlesEditor/modal/ShiftSubtitlesModal.svelte';
 import AudioCutterModal from '../projectEditor/tabs/toolsEditor/modal/AudioCutterModal.svelte';
+import AudioMergeModal from '../projectEditor/tabs/toolsEditor/modal/AudioMergeModal.svelte';
 import { type UpdateInfo } from '$lib/services/VersionService.svelte';
 
 export default class ModalManager {
@@ -198,6 +199,28 @@ export default class ModalManager {
 
 			// Monter le composant Svelte 5
 			const modal = mount(AudioCutterModal, {
+				target: container,
+				props: {
+					close: () => {
+						// Nettoyer et résoudre
+						unmount(modal);
+						document.body.removeChild(container);
+						resolve();
+					}
+				}
+			});
+		});
+	}
+
+	static async audioMergeModal(): Promise<void> {
+		return new Promise<void>((resolve) => {
+			// Créer un conteneur pour le modal
+			const container = document.createElement('div');
+			container.classList.add('modal-wrapper');
+			document.body.appendChild(container);
+
+			// Monter le composant Svelte 5
+			const modal = mount(AudioMergeModal, {
 				target: container,
 				props: {
 					close: () => {

6d21550a6ca205831c9fbe0ad8f2ae51eafc1df3
Rayane STASZEWSKI <zonetecde@gmail.com>
2026-02-15 12:33:30 +0100

    Added "To Review" status

--- src/lib/classes/Status.ts
+++ src/lib/classes/Status.ts
@@ -4,6 +4,7 @@ export class Status extends SerializableBase {
 	static readonly NOT_SET = new Status('Not Set', '#ffffff');
 	static readonly TO_CAPTION = new Status('To Caption', '#ffea00');
 	static readonly TO_TRANSLATE = new Status('To Translate', '#c97b14');
+	static readonly TO_REVIEW = new Status('To Review', '#00b3ff');
 	static readonly TO_EXPORT = new Status('To Export', '#e600ff');
 	static readonly EXPORTED = new Status('Exported', '#11ff00');
 
@@ -21,6 +22,7 @@ export class Status extends SerializableBase {
 			Status.NOT_SET,
 			Status.TO_CAPTION,
 			Status.TO_TRANSLATE,
+			Status.TO_REVIEW,
 			Status.TO_EXPORT,
 			Status.EXPORTED
 		];

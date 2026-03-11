a8cd47129c6e17ccc37b70cc781243cfb633fc01
Rayane STASZEWSKI <zonetecde@gmail.com>
2026-03-11 19:38:36 +0100

    reverted a logic

--- src/lib/classes/Exporter.ts
+++ src/lib/classes/Exporter.ts
@@ -211,24 +211,24 @@ export default class Exporter {
 
 		// Créer une fenêtre Tauri avec la bonne taille
 		const w = new WebviewWindow(exportId, {
-			// center: false,
-			// decorations: false,
-			// visible: true,
-			// focus: false,
-			// skipTaskbar: true,
-			// preventOverflow: false,
-			// x: -10000,
-			// y: -10000,
-			// backgroundThrottling: 'disabled' as BackgroundThrottlingPolicy,
-			// alwaysOnTop: false,
-			// alwaysOnBottom: true,
+			center: false,
+			decorations: false,
+			visible: true,
+			focus: false,
+			skipTaskbar: true,
+			preventOverflow: false,
+			x: -10000,
+			y: -10000,
+			backgroundThrottling: 'disabled' as BackgroundThrottlingPolicy,
+			alwaysOnTop: false,
+			alwaysOnBottom: true,
 			title: 'QC - ' + exportId,
 			url: '/exporter?' + new URLSearchParams({ id: exportId }) // Met en paramètre l'ID de l'export pour que l'exportateur puisse le récupérer
 		});
 
 		w.once('tauri://created', async () => {
 			try {
-				// await w.setPosition(new LogicalPosition(-10000, -10000));
+				await w.setPosition(new LogicalPosition(-10000, -10000));
 			} catch (error) {
 				console.warn('Unable to move export window off-screen:', error);
 			}

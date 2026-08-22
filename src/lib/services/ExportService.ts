46af099c4be390f185aa466bd3f746ac65347690
Rayane Staszewski <56195432+zonetecde@users.noreply.github.com>
2026-08-22 23:16:00 +0200

    fix(exports): only cancel exports owned by closing window

--- src/lib/services/ExportService.ts
+++ src/lib/services/ExportService.ts
@@ -191,7 +191,9 @@ export default class ExportService {
 	}
 
 	static currentlyExportingProjects() {
-		return globalState.exportations.filter((exp) => exp.isOnGoing());
+		return globalState.exportations.filter(
+			(exp) => exp.isOnGoing() && this.ownedExportIds.has(exp.exportId)
+		);
 	}
 }
 
